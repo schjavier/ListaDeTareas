69cbc970e2455bf2d592d9654898cf09ce168b67
Javier <schjavier@gmail.com>
2022-09-30 08:42:06 -0300

    borrar y actualizar datos de tareas

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHandler} from '@angular/common/http'
+import { HttpClient, HttpHandler, HttpHeaders} from '@angular/common/http'
 import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASK } from 'src/app/mock-task';
+
+const httpOptions={
+  headers: new HttpHeaders({'Content-Type':'application/json'})
+}
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +19,15 @@ export class TaskService {
   getTask(): Observable<Task[]>{
     return this.http.get<Task[]>(this.apiUrl)
 
+  }
+
+  deleteTask(task:Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder (task:Task) : Observable<Task>{
+     const url= `${this.apiUrl}/${task.id}`;
+     return this.http.put<Task>(url, task, httpOptions)
   }
 }
